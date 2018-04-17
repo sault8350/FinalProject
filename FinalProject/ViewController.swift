df15bc3793101d72914c114cc7e66c9f3fbc958c
Samuel Ault <sault8350@jh-mba158-01.d214.org>
2018-04-17 10:36:52 -0500

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Samuel Ault on 4/17/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
